348e7eaae170b85af645457b979e8348ab42b897
rcabebe <rcabebe@woofy.com.ph>
2024-10-13 04:08:17 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React, { Suspense, lazy, useState } from "react";
-import LoadingScreen from "./components/LoadingScreen";
 const LandingPage = lazy(() => import("./pages/LandingPage"));
 
 function App() {
   return (
-    <Suspense fallback={<LoadingScreen />}>
+    <Suspense>
       <LandingPage />
     </Suspense>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "../src/assets/css/app.css";
 import "../src/assets/css/button.css";
-import "../src/assets/css/loading-screen.css";
 import "../src/assets/css/font.css";
 import "../src/assets/css/picture.css";
 import "../src/assets/css/random-pop-up.css";
 import "../src/assets/css/button.css";
 import "../src/assets/css/say-yes.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+const rootElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
